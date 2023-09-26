9ebbf661387939068aaa0cef054ab7d38ccdd97c
Michael <michaelpage05@hotmail.co.uk>
2023-09-26 11:04:34 +0100

    added aliase to open phpstom

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -10,4 +10,5 @@ alias pencil='pencil --disable-seccomp-filter-sandbox'
 # Repros
 alias repos='cd ~/data-drive/repos/' #All repros
 
-
+# PHPstorm
+alias phpstorm='/home/michael/.local/share/flatpak/app/com.jetbrains.PhpStorm/x86_64/stable/c7f8b03c905d8e6191a25b44b80d84a22b1d1810a52c46c13baeeeda7c1e17db/files/extra/phpstorm/bin/phpstorm.sh .'

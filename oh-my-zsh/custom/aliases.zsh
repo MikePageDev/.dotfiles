3c472d1ebbec3fd20dfda5197252df972118a243
Michael <Michaelpage05@hotmail.co.uk>
2023-09-27 17:25:17 +0100

    added vim bindings

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -11,4 +11,4 @@ alias pencil='pencil --disable-seccomp-filter-sandbox'
 alias repos='cd ~/data-drive/repos/' #All repros
 
 # PHPstorm
-alias phpstorm='/home/michael/.local/share/flatpak/app/com.jetbrains.PhpStorm/x86_64/stable/c7f8b03c905d8e6191a25b44b80d84a22b1d1810a52c46c13baeeeda7c1e17db/files/extra/phpstorm/bin/phpstorm.sh .'
+alias phpstorm='/home/michael/.local/share/JetBrains/Toolbox/apps/PhpStorm/ch-0/231.8770.68/bin/phpstorm.sh .'
